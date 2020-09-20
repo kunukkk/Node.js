1d7bca32fed6ccf6843241619d95c27398b8d3c5
kunukkk <showklee@naver.com>
2020-09-20 21:20:09 +0900

    2020/09/20_day_3
    node basic functions

---
+++ ch04/ch04_test6.js
@@ -0,0 +1,9 @@
+let fs = require("fs");
+
+// 파일을 비동기식 IO로 읽어 들입니다
+fs.readFile("./package.json", "utf8", function (err, data) {
+  // 일어 들인 데이터 출력
+  console.log(data);
+});
+
+console.log("프로젝트 폴더 안의 package.json 파일을 읽도록 요청했습니다.");
