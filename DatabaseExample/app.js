b3876b19824db62f64a535e637ef13d043a29195
kunukkk <showklee@naver.com>
2020-09-29 17:46:53 +0900

    20/09/29_day_6
    use database with MongoDB

--- DatabaseExample/app.js
+++ DatabaseExample/app.js
@@ -43,51 +43,89 @@ app.use(
 );
 
 // 몽고디비 모듈 사용
-const MongoClient = require('mongodb').MongoClient;
+const MongoClient = require("mongodb").MongoClient;
 
 // 데이버베이스 객체를 위한 변수 선언
-const database;
+let database;
 
 // 데이터베이스에 연결
-function connectDB(){
-    // 데이터베이스 연결 정보
-    const databaseUrl = 'mongodb://localhost:27017/local';
+function connectDB() {
+  // 데이터베이스 연결 정보
+  const databaseUrl = "mongodb://localhost:27017/local";
 
-    // 데이터베이스 연결
-    MongoClient.connect(databaseUrl, function(err, db){
-        if(err) throw err;
+  // 데이터베이스 연결
+  MongoClient.connect(databaseUrl, function (err, db) {
+    if (err) throw err;
 
-        console.log('데이터베이스에 연결되었습니다. : ' + databaseUrl);
+    console.log("데이터베이스에 연결되었습니다. : " + databaseUrl);
 
-        // database 변수에 할당
-        database = db;
-    })
+    // database 변수에 할당
+    database = db;
+  });
 }
 
 // 사용자를 인증하는 함수
-const authUser = function(database, id, password, callback){
-    console.log('authUser 호출됨');
+const authUser = function (database, id, password, callback) {
+  console.log("authUser 호출됨");
 
-    // users 컬렉션 참조
-    const users = database.collection('users');
+  // users 컬렉션 참조
+  let users = database.collection("users");
 
-    // 아이디와 비밀번호를 사용해 검색
-    users.find({'id':id, 'password':password}).toArray(function(err, docs){
-        if(err){
-            callback(err, null);
-            return;
-        }
-        
-        if(docs.length > 0){
-            console.log('아이디 [%s], 비밀번호 [%s]가 일치하는 사용자 찾음', id, password);
-            callback(null, docs);
-        }
-        else{
-            console.log('일치하는 사용자를 찾지 못함');
-            callback(null, null);
-        }
-    })
-}
+  // 아이디와 비밀번호를 사용해 검색
+  users.find({ id: id, password: password }).toArray(function (err, docs) {
+    if (err) {
+      callback(err, null);
+      return;
+    }
+
+    if (docs.length > 0) {
+      console.log(
+        "아이디 [%s], 비밀번호 [%s]가 일치하는 사용자 찾음",
+        id,
+        password
+      );
+      callback(null, docs);
+    } else {
+      console.log("일치하는 사용자를 찾지 못함");
+      callback(null, null);
+    }
+  });
+};
+
+app.post("/process/login", function (req, res) {
+  console.log("/process/login 호출됨");
+
+  const paramID = req.params.id || req.query.id;
+  const paramPassword = req.params.password || req.query.password;
+
+  if (database) {
+    authUser(database, paramID, paramPassword, function (err, docs) {
+      if (err) throw err;
+
+      if (docs) {
+        console.dir(docs);
+        let username = docs[0].name;
+        res.writeHead("200", { "Content-Type": "text/html; charset=utf8" });
+        res.write("<h1>로그인 성공</h1>");
+        res.write("<div><p>사용자 아이디 : " + paramID + "</p></div>");
+        res.write("<div><p>사용자 이름 : " + username + "</p></div>");
+        res.write("<br><br><a href='/public/login.html'>다시 로그인하기</a>");
+        res.end();
+      } else {
+        res.writeHead("200", { "Content-Type": "text/html; charset=utf8" });
+        res.write("<h1>로그인 실패</h1>");
+        res.write("<div><p>아이디와 비밀번호를 다시 확인하세요</p></div>");
+        res.write("<br><br><a href='/public/login.html'>다시 로그인하기</a>");
+        res.end();
+      }
+    });
+  } else {
+    res.writeHead("200", { "Content-Type": "text/html; charset=utf8" });
+    res.write("<h2>데이터베이스 연결 실패</h2>");
+    res.write("<div><p>데이터베이스에 연결하지 못했습니다</p></div>");
+    res.end();
+  }
+});
 
 // 라우터 객체 참조
 const router = express.Router();
