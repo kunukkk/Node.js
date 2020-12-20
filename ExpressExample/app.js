123d6f8236f6c6bddc5a652bdfcf17dce777705a
ukunV <showklee@naver.com>
2020-12-20 19:30:34 +0900

    20/12/20_update
    20/12/20_update

--- ExpressExample/app.js
+++ ExpressExample/app.js
@@ -4,7 +4,7 @@ const http = require("http");
 // express 모듈은 웹 서버를 위해 만들어진 것으로 http 모듈 위에서 동작함
 // 따라서 express 모듈을 사용할 때는 항상 http 모듈도 함께 불러들여야 함
 
-// Express 객체 생성
+// Express 서버 객체 생성
 const app = express();
 
 // 기본 포트를 app 객체에 속성으로 설정
