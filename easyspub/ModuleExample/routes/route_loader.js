c0b831a5483f4f385ca06e78380e6a9ad51d4ad0
ukunV <showklee@naver.com>
2021-02-14 17:16:06 +0900

    21/02/14_review
    21/02/14_review

--- easyspub/ModuleExample/routes/route_loader.js
+++ easyspub/ModuleExample/routes/route_loader.js
@@ -2,41 +2,37 @@ let route_loader = {};
 
 const config = require('../config');
 
-
-route_loader.init = function(app, router) {
-	console.log('route_loader.init 호출됨.');
-	return initRoutes(app, router);
-}
+route_loader.init = function (app, router) {
+  console.log('route_loader.init 호출됨.');
+  return initRoutes(app, router);
+};
 
 // route_info에 정의된 라우팅 정보 처리
 function initRoutes(app, router) {
+  let infoLen = config.route_info.length;
+  console.log('설정에 정의된 라우팅 모듈의 수 : %d', infoLen);
 
-	let infoLen = config.route_info.length;
-	console.log('설정에 정의된 라우팅 모듈의 수 : %d', infoLen);
- 
-	for (let i = 0; i < infoLen; i++) {
-		let curItem = config.route_info[i];
-			
-		// 모듈 파일에서 모듈 불러옴
-		let curModule = require(curItem.file);
-		console.log('%s 파일에서 모듈정보를 읽어옴.', curItem.file);
-		
-		//  라우팅 처리
-		if (curItem.type == 'get') {
-            router.route(curItem.path).get(curModule[curItem.method]);
-		} else if (curItem.type == 'post') {
-            router.route(curItem.path).post(curModule[curItem.method]);
-		} else {
-			router.route(curItem.path).post(curModule[curItem.method]);
-		}
-		
-		
-		console.log('라우팅 모듈 [%s]이(가) 설정됨.', curItem.method);
-	}
+  for (let i = 0; i < infoLen; i++) {
+    let curItem = config.route_info[i];
 
-    // 라우터 객체 등록
-    app.use('/', router);
+    // 모듈 파일에서 모듈 불러옴
+    let curModule = require(curItem.file);
+    console.log('%s 파일에서 모듈정보를 읽어옴.', curItem.file);
+
+    //  라우팅 처리
+    if (curItem.type == 'get') {
+      router.route(curItem.path).get(curModule[curItem.method]);
+    } else if (curItem.type == 'post') {
+      router.route(curItem.path).post(curModule[curItem.method]);
+    } else {
+      router.route(curItem.path).post(curModule[curItem.method]);
+    }
+
+    console.log('라우팅 모듈 [%s]이(가) 설정됨.', curItem.method);
+  }
+
+  // 라우터 객체 등록
+  app.use('/', router);
 }
 
 module.exports = route_loader;
-
