eb16f629fe66c54e94588f555f27742f1beb07d6
ukunV <showklee@naver.com>
2021-02-23 14:04:28 +0900

    21/02/23_update
    21/02/23_update

--- node-clone/topicList/routes/add.js
+++ node-clone/topicList/routes/add.js
@@ -5,6 +5,7 @@ const mySqlClient = mysql.createConnection(require("../config/db_config"));
 const add = (req, res) => {
   res.render("add.html", {});
 };
+
 const add_complete = (req, res) => {
   const addQuery =
     "INSERT INTO topics(name, description, author) select ?, ?, authors.id FROM authors WHERE NAME=?;";
