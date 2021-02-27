5f1f0683405600301b44af832cdcb2ce8430e2bd
ukunV <showklee@naver.com>
2021-02-27 15:51:04 +0900

    use_sequelize
    use_sequelize

--- node-clone/topicList/models/user.js
+++ node-clone/topicList/models/user.js
@@ -2,11 +2,6 @@ module.exports = (sequelize, DataTypes) => {
   return sequelize.define(
     "user",
     {
-      id: {
-        type: DataTypes.STRING(50),
-        allowNull: false,
-        primaryKey: true,
-      },
       password: {
         type: DataTypes.STRING(100),
         allowNull: false,
--- node-clone/topicList/routes/login.js
+++ node-clone/topicList/routes/login.js
@@ -1,7 +1,9 @@
-const mysql = require("mysql");
+// const mysql = require("mysql");
 const crypto = require("crypto");
 
-const mySqlClient = mysql.createConnection(require("../config/db_config"));
+// const mySqlClient = mysql.createConnection(require("../config/db_config"));
+
+const models = require("../models");
 
 const login = (req, res) => {
   if (req.session.user) {
@@ -11,19 +13,47 @@ const login = (req, res) => {
   }
 };
 
-const login_check = (req, res) => {
-  const loginQuery = "select id, name from users where id=? and password=?";
-  const password = crypto
-    .createHash("sha512")
-    .update(req.body.password)
-    .digest("base64");
-  mySqlClient.query(loginQuery, [req.body.id, password], (err, row) => {
-    if (err) console.log(err);
-    else {
-      if (row[0]) {
+const login_check = (req, res, next) => {
+  // const loginQuery = "select id, name from users where id=? and password=?";
+  // const password = crypto
+  //   .createHash("sha512")
+  //   .update(req.body.password)
+  //   .digest("base64");
+  // mySqlClient.query(loginQuery, [req.body.id, password], (err, row) => {
+  //   if (err) console.log(err);
+  //   else {
+  //     if (row[0]) {
+  //       req.session.user = {
+  //         id: row[0].id,
+  //         name: row[0].name,
+  //       };
+  //       res.redirect("/home");
+  //     } else {
+  //       res.send(
+  //         '<script type="text/javascript">alert("아이디 또는 비밀번호가 일치하지 않습니다."); window.location="/";</script>'
+  //       );
+  //     }
+  //   }
+  // });
+  models.User.findOne({
+    attributes: ["id", "name"],
+    where: [
+      { id: req.body.id },
+      {
+        password: crypto
+          .createHash("sha512")
+          .update(req.body.password)
+          .digest("base64"),
+      },
+    ],
+    raw: true,
+  })
+    .then((row) => {
+      console.log(row);
+      if (row) {
         req.session.user = {
-          id: row[0].name,
-          name: row[0].name,
+          id: row.id,
+          name: row.name,
         };
         res.redirect("/home");
       } else {
@@ -31,8 +61,11 @@ const login_check = (req, res) => {
           '<script type="text/javascript">alert("아이디 또는 비밀번호가 일치하지 않습니다."); window.location="/";</script>'
         );
       }
-    }
-  });
+    })
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
 };
 
 module.exports = { login, login_check };
